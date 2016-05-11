6d57b5294c30f6d35b2633b30a6c20c577ea5322
danozgriff <dane.griff@gmail.com>
2016-05-11 17:42:56 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -4,7 +4,7 @@ import lxml
 u=urllib2.urlopen("http://pilbaraports.com.au/Shipping_Schedule/Current_Shipping_Schedule.pdf")
  
 x=scraperwiki.pdftoxml(u.read())
-print x[:1024]
+print x
 r=lxml.etree.fromstring(x)
 r.xpath('//page[@number="1"]')
 r.xpath('//text[@left="64"]/b')[0:10]
