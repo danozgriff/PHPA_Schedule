eaee3571443d42763e5aaebe339842c7bd8254ce
danozgriff <dane.griff@gmail.com>
2016-05-11 17:30:22 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,8 +1,14 @@
-import scraperwiki import urllib2, lxml.etree
+import scraperwiki 
+import urllib2, lxml.etree
 
 url = 'http://pilbaraports.com.au/Shipping_Schedule/Current_Shipping_Schedule.pdf'
 pdfdata = urllib2.urlopen(url).read() 
 xmldata = scraperwiki.pdftoxml(pdfdata) 
 root = lxml.etree.fromstring(xmldata)
+
+pages = list(root) 
+print "There are",len(pages),"pages"
+
 print etree.tostring(root, pretty_print=True)
-pages = list(root) print "There are",len(pages),"pages"
+
+
