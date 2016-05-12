e1ce0e9a8100c1a8dc976b5b96434bdf88b8b612
danozgriff <dane.griff@gmail.com>
2016-05-12 11:21:55 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -5,10 +5,15 @@ u=urllib2.urlopen("http://pilbaraports.com.au/Shipping_Schedule/Current_Shipping
  
 x=scraperwiki.pdftoxml(u.read())
 print x
-r=lxml.etree.fromstring(x)
-r.xpath('//page[@number="1"]')
-r.xpath('//text[@left="64"]/b')[0:10]
-r.xpath('//text[@left="64"]/b')[8].text
+#r=lxml.etree.fromstring(x)
+#r.xpath('//page[@number="1"]')
+#r.xpath('//text[@left="64"]/b')[0:10]
+#r.xpath('//text[@left="64"]/b')[8].text
+
+#html = response.read()
+#test1 = re.search(r'Day\'s Volume(.*?)<br \/><\/div>', html).group()
+#tuples = re.findall(r'((\">|\'>)(.*?)<\/))', str(test1.replace(" ", "")).replace("><", ""))
+#tuples = re.findall(r'(\">|\'>|img\/)(.*?)(<\/|\.gif)', str(test1.replace(" ", "")).replace("><", ""))
 
 
 
