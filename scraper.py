b12680620afd7c33f534e0235be10d8b00b4abf1
danozgriff <dane.griff@gmail.com>
2016-05-12 11:50:31 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -4,16 +4,24 @@ import lxml
 u=urllib2.urlopen("http://pilbaraports.com.au/Shipping_Schedule/Current_Shipping_Schedule.pdf")
  
 x=scraperwiki.pdftoxml(u.read())
-print x
+#print x
 #r=lxml.etree.fromstring(x)
 #r.xpath('//page[@number="1"]')
 #r.xpath('//text[@left="64"]/b')[0:10]
 #r.xpath('//text[@left="64"]/b')[8].text
 
 #html = response.read()
-#test1 = re.search(r'Day\'s Volume(.*?)<br \/><\/div>', html).group()
-#tuples = re.findall(r'((\">|\'>)(.*?)<\/))', str(test1.replace(" ", "")).replace("><", ""))
+#test1 = re.search(r'(.*?)<br \/><\/div>', x).group()
+tuples = re.findall(r'((left="|width="|<b>)(.*?)(</b>|"))', x)
+for tuple in tuples:
+ print tuple[1]
+ print tuple[2]
+ print tuple[3]
+#str(test1.replace(" ", "")).replace("><", ""))
 #tuples = re.findall(r'(\">|\'>|img\/)(.*?)(<\/|\.gif)', str(test1.replace(" ", "")).replace("><", ""))
+
+
+#<text top="97" left="28" width="329" height="19" font="0"><b>THURSDAY 12th MAY 2016          </b></text>
 
 
 
