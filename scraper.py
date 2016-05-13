db898bf37cf1d5dbef0ef8904d871673aa58e532
danozgriff <dane.griff@gmail.com>
2016-05-13 08:28:05 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -55,44 +55,44 @@ for tuple in tuples:
   cnt=cnt+1
 
    
+ if headers==0:
+  if cnt == 0:
+   obj = tuple[2].strip(' ')
+   cnt=cnt+1
+  elif cnt == 1:
+   #y=tuple[2].find('  ')+1
+   #obj = obj + '|||' + tuple[2][:y]
+   obj = obj + '|' + tuple[2].strip(' ')
+   cnt=0
 
- if cnt == 0:
-  obj = tuple[2].strip(' ')
-  cnt=cnt+1
- elif cnt == 1:
-  #y=tuple[2].find('  ')+1
-  #obj = obj + '|||' + tuple[2][:y]
-  obj = obj + '|' + tuple[2].strip(' ')
-  cnt=0
-
   
   
-  print obj
-  print obj.find('|')
-  #print int(obj[:obj.find('|')])
+   print obj
+   print obj.find('|')
+   #print int(obj[:obj.find('|')])
   
-  if (delim==0 and int(obj[:obj.find('|')]) < DWT):
-   record = re.search(r'\|((.|\n)+)', obj).group(0)
-   print record
+   if (delim==0 and int(obj[:obj.find('|')]) < DWT):
+    record = re.search(r'\|((.|\n)+)', obj).group(0)
+    print record
 
    
-   row = obj
-   obj = ''
-  else:
-   row = row + obj
-   obj = ''
-  
-  #sys.stdout.write('|') 
- cnt=cnt+1
-# sys.stdout.write(tuple[2])
- #print(tuple[2],end="")
- #,flush=True
-# print tuple[3]
-#str(test1.replace(" ", "")).replace("><", ""))
-#tuples = re.findall(r'(\">|\'>|img\/)(.*?)(<\/|\.gif)', str(test1.replace(" ", "")).replace("><", ""))
-
-
-#<text top="97" left="28" width="329" height="19" font="0"><b>THURSDAY 12th MAY 2016          </b></text>
-
+    row = obj
+    obj = ''
+   else:
+    row = row + obj
+    obj = ''
+   
+   #sys.stdout.write('|') 
+  #cnt=cnt+1
+ # sys.stdout.write(tuple[2])
+  #print(tuple[2],end="")
+  #,flush=True
+ # print tuple[3]
+ #str(test1.replace(" ", "")).replace("><", ""))
+ #tuples = re.findall(r'(\">|\'>|img\/)(.*?)(<\/|\.gif)', str(test1.replace(" ", "")).replace("><", ""))
+ 
+ 
+ #<text top="97" left="28" width="329" height="19" font="0"><b>THURSDAY 12th MAY 2016          </b></text>
+ 
 
 
