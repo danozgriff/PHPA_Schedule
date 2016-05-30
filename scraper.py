97752cb5c6b696b67d8895816022026b86f27c9c
danozgriff <dane.griff@gmail.com>
2016-05-30 16:24:08 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -13,8 +13,8 @@ print Schedule_Date
 
 # Scan PDF
 test1 = re.search(r'Duty Helo:((.|\n)+)TIDES', x).group(0)
-#print test1
-#test1 = re.search(r'Day\'s Volume(.*?)<br \/><\/div>', html).group()
+print test1
+
 tuples = re.findall(r'((left="|">)(.*?)(</text>|"))', test1.replace('<b>', '').replace('</b>', ''))
 cnt=0
 obj = ''
