8bcec97fa0b31915657d7091c48c80fbedcfeb1c
danozgriff <dane.griff@gmail.com>
2016-05-31 14:53:50 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -127,6 +127,7 @@ for tuple in tuples:
    #print loc
 
    while alignment==0: 
+    print 'TESTING HDLIST Loc Val:' + HeadersList[colcnt]
     if (loc >= HeadersList[colcnt]-5 and loc <= HeadersList[colcnt]+5) or vesselflag==1:
      lineout = lineout + obj + ','
      print 'CONFIRMED: ' + 'loc: ' + str(loc) + ' obj: ' + obj + ' LOCKED TO: ' + str(HeadersList[colcnt])
