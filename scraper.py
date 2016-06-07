5ac706ccce7842fb93ec350d58470ac01ba71d8b
danozgriff <dane.griff@gmail.com>
2016-06-07 14:04:56 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -13,7 +13,7 @@ print Schedule_Date
 
 # Scan PDF
 test1 = re.search(r'Duty Helo:((.|\n)+)TIDES', x).group(0)
-print test1
+#print test1
 
 tuples = re.findall(r'((left="|">)(.*?)(</text>|"))', test1.replace('<b>', '').replace('</b>', ''))
 #cnt=0
@@ -46,7 +46,7 @@ for tuple in tuples:
      loc = float(tuple[2].strip(' '))
      recflag = 0
   
-  elif runcnt==34:
+  elif runcnt==30:
     headerrow=0
   
   
